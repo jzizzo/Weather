25e7c2e5335eaa37aef7f704ab70764c4897f1c6
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-30 19:11:30 -0700

    add googleMaps intergration and remove console logs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${window.API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${window.API_KEY}`;
 
-export const FETCH_WEATHER = 'FETCH_WEATHER'
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-
-  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
